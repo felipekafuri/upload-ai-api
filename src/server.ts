12f2a441aa44d48a7f08b682d72a3f79463b2353
Felipe Kafuri <felipe11.rk@gmail.com>
2023-09-14 09:01:18 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,22 @@ import { generateAiCompletionRoute } from './routes/generate-ai-compilation'
 
 const app = fastify()
 
-
+app.register(fastifyCors,{
+  origin: '*'
+})
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
 app.register(generateAiCompletionRoute)
 
+
+
 app.listen({
   port: 3000,
-  host: '0.0.0.0'
 }, () => {
   console.log(`Server listening at http://localhost:3000`)
 })
+function fastifyCors(instance: FastifyInstance<RawServerDefault, IncomingMessage, ServerResponse<IncomingMessage>, FastifyBaseLogger, FastifyTypeProvider>, opts: FastifyPluginOptions, done: (err?: Error | undefined) => void): void {
+  throw new Error('Function not implemented.')
+}
+
